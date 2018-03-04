4ef9fcfce6ab28696e76ebf0c19004343a34c68f
ericma000 <ericma000@gmail.com>
2018-03-03 23:37:30 -0800

    highlight path

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+    $(document).ready(function() {
 
 
     var related_gene = 
@@ -14,7 +14,19 @@ $(document).ready(function() {
             [276, 235, 232, 228, 245, 232, 335, 431, 0]   // m.jan
         ]
 
-    console.log(related_gene);
+    var com_ancestors =
+        [
+            [0, 97.5, 320.5, 320.5, 355.7, 847, 1514, 1514, 4290],
+            [97.5, 0, 320.5, 320.5, 355.7, 847, 1514, 1514, 4290],
+            [320.5, 320.5, 0, 252.6, 355.7, 847, 1514, 1514, 4290],
+            [320.5, 320.5, 252.6, 0, 355.7, 847, 1514, 1514, 4290],
+            [355.7, 355.7, 355.7, 355.7, 0, 847, 1514, 1514, 4290],
+            [847, 847, 847, 847, 847, 0, 1514, 1514, 4290],
+            [1514,  1514, 1514, 1514, 1514, 1514, 0, 557, 4290],
+            [1514, 1514, 1514, 1514, 1514, 1514, 551, 0, 4290],
+            [4290, 4290, 4290, 420, 4290, 4290, 4290, 4290, 0]
+        ]
+
 
     var state = 0;
 
@@ -36,7 +48,7 @@ $(document).ready(function() {
     var c1;
     var c2;
 
-    d3.json('../data/data.txt', function(data) {
+    d3.json('../data/data.json', function(data) {
 
         var nodes = tree.nodes(data);     
         var links = tree.links(nodes);
@@ -48,11 +60,8 @@ $(document).ready(function() {
                 .attr('class', 'node')
                 .attr('transform', function(d) { return 'translate(' + (d.y + 300)  + ',' + (d.x + 100) + ')';})
                 .on('click', function(d) {
-                    show_descrip(d);
+                    show_descrip(d, links);
                 })
-
-
-
 
         node.append('circle')
             .attr('r', function(d) {return d.radius})
@@ -122,9 +131,14 @@ $(document).ready(function() {
 
     function show_descrip(data) {
 
+
+        highlight_link(data, data.depth, data.color);
+
+
         if (state == 0) {
 
             c1 = data;
+
 
             $('#sp_1 h4').text(data.name.toUpperCase());            
             $('#sp_1 p').text(data.description);
@@ -200,6 +214,9 @@ $(document).ready(function() {
                     })
                 }
 
+                // unhighlight tree
+                unhighlight_link(data, data.depth);
+
             } else {
 
                 if (empty_display == '#sp_1') {
@@ -224,6 +241,7 @@ $(document).ready(function() {
                    opacity: 1
                 })
 
+                // append circles
                 svg.append('circle')
                     .attr('id', 'species_1')
                     .attr('cx', '850px')
@@ -255,11 +273,18 @@ $(document).ready(function() {
                 setTimeout(function() {
                     d3.select('#rg_left h5')
                         .text(related_gene[c1.id][c2.id]);
+
+                    d3.select('#com_ances span')
+                        .text(com_ancestors[c1.id][c2.id]);
                 }, 250)
  
-
-
                 d3.select('#rg_right')
+                    .transition()
+                    .duration(1000)
+                    .style('opacity', 1);
+
+                // appeend common_ancestor
+                d3.select('#com_ances')
                     .transition()
                     .duration(1000)
                     .style('opacity', 1);
@@ -282,6 +307,10 @@ $(document).ready(function() {
                    opacity: 1
                 })
 
+                //unhighlight links
+                unhighlight_link(data, data.depth);
+                highlight_link(c2, c2.depth, c2.color);
+
             } else if (data.name.toUpperCase() == $('#sp_2 h4').text()) {
                 $('#sp_2 h4').empty();
                 $('#sp_2 p').empty();
@@ -296,6 +325,10 @@ $(document).ready(function() {
                    top: '490',
                    opacity: 0
                 })
+
+                //unhighlight links
+                unhighlight_link(data, data.depth);
+                highlight_link(c1, c1.depth, c1.color);
 
             } else {
                 c1 = data;
@@ -317,6 +350,10 @@ $(document).ready(function() {
                    top: '490px',
                    opacity: 0
                 })
+
+                unhighlight_link(data, 0);
+                highlight_link(data, data.depth, data.color);
+
             }
 
             d3.select('#species_1')
@@ -341,22 +378,77 @@ $(document).ready(function() {
                 .duration(500)
                 .style('opacity', 0);
 
-
+            // appeend common_ancestor
+            d3.select('#com_ances')
+                .transition()
+                .duration(500)
+                .style('opacity', 0);
 
             state = 0;
 
         }
 
 
+        state++;
         console.log(c1);
         console.log(c2);
 
 
-        state++;
         console.log('after: ' + state);
 
     }
 
+
+
+
+    function highlight_link(node, depth, color) {
+
+        var source_node;
+        if (depth == 1) {
+            d3.selectAll(".link")
+                .filter(function(d) {
+                    return d.target === node;
+                })
+                .style("stroke", color)
+        } else {
+            d3.selectAll(".link")
+                .filter(function(d) {
+                    if (d.target === node) {
+                        source_node = d.source;
+                        return d;
+                    }
+                })
+                .style("stroke", color);
+
+            highlight_link(source_node, source_node.depth, color);
+        }
+    }
+
+    function unhighlight_link(node, depth) {
+
+        var source_node;
+        if (depth == 0) {
+            d3.selectAll('.link')
+                .style('stroke', 'black');
+        } else if (depth == 1) {
+            d3.selectAll(".link")
+                .filter(function(d) {
+                    return d.target === node;
+                })
+                .style("stroke", 'black')
+        } else {
+            d3.selectAll(".link")
+                .filter(function(d) {
+                    if (d.target === node) {
+                        source_node = d.source;
+                        return d;
+                    }
+                })
+                .style("stroke", 'black');
+
+            unhighlight_link(source_node, source_node.depth);
+        }
+    }
 
 
 
